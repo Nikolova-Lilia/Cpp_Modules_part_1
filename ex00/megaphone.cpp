41535d0aa9b2ce36c851a20973501e212d2e7a22
Nikolova-Lilia <nikolovalilianenkova@gmail.com>
2024-08-19 13:46:44 +0200

    megaphone - first task completed successfully

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    int i;
+    int j;
+
+    j = 1;
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    while (argc > 1 && argv[j])
+    {
+        i = 0;
+        while (argv[j][i])
+        {
+            std::cout << (char)std::toupper(argv[j][i]);
+            i++;
+        }
+        j++;
+    }
+    std::cout << std::endl;
+    return (0);
+}
